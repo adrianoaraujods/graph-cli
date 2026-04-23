4a47da6dba0074adef686f396ab041f6ae46839c
Adriano Araújo <adrianoaraujodsantos@hotmail.com>
2026-04-23 17:12:25 -0300

    perf(NaiveBridges): optimized using parallel streams and batching on edges

--- src/main/java/graph/algorithms/NaiveBridges.java
+++ src/main/java/graph/algorithms/NaiveBridges.java
@@ -1,31 +1,83 @@
 package graph.algorithms;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import graph.api.Graph;
-import graph.api.GraphBase;
 import graph.api.GraphBase.IteratorVisitor;
 import graph.api.UndirectedGraph;
 import graph.util.EdgeFormatter;
+import graph.util.UnionFind;
 
 public class NaiveBridges {
 
-  public static Set<String> findAll(Graph graph) {
-    Set<String> bridges = new HashSet<>();
+  private static class EdgeBatch {
+    private final int[][] edges;
+    private final int componentsCount;
 
-    int componentsCount = ConnectedComponents.getCount(graph);
+    EdgeBatch(int[][] edges, int start, int end, int componentsCount) {
+      this.edges = Arrays.copyOfRange(edges, start, end);
+      this.componentsCount = componentsCount;
+    }
 
-    GraphBase.IteratorVisitor iterator = new IteratorVisitor() {
-      @Override
-      public void examineEdge(int v, int w) {
-        if (ConnectedComponents.getCount(graph, new int[] { v, w }) != componentsCount) {
-          bridges.add(EdgeFormatter.toKey(v, w));
+    Set<String> compute(Graph graph) {
+      Set<String> localBridges = new HashSet<>();
+
+      for (int[] edge : edges) {
+        int v = edge[0];
+        int w = edge[1];
+
+        UnionFind uf = new UnionFind(graph.getVerticesCount());
+
+        IteratorVisitor iterator = new IteratorVisitor() {
+          @Override
+          public void examineEdge(int edgeV, int edgeW) {
+            if (edgeV == v && edgeW == w) {
+              return;
+            }
+            uf.union(edgeV - 1, edgeW - 1);
+          }
+        };
+
+        graph.iterateGraph(iterator);
+
+        if (uf.getCount() != componentsCount) {
+          localBridges.add(EdgeFormatter.toKey(v, w));
         }
       }
-    };
 
-    graph.iterateGraph(iterator);
+      return localBridges;
+    }
+  }
+
+  public static Set<String> findAll(Graph graph) {
+    int componentsCount = ConnectedComponents.getCount(graph);
+
+    int[][] edges = graph.getEdgesSet();
+    int m = edges.length;
+
+    if (m == 0) {
+      return new HashSet<>();
+    }
+
+    int numThreads = Runtime.getRuntime().availableProcessors();
+    int batchSize = Math.max(1, m / numThreads);
+
+    Set<String> bridges = Arrays.stream(IntStream.range(0, numThreads).toArray())
+        .parallel()
+        .mapToObj(i -> {
+          int start = i * batchSize;
+          int end = (i == numThreads - 1) ? m : Math.min((i + 1) * batchSize, m);
+          if (start >= m) {
+            return new HashSet<String>();
+          }
+          return new EdgeBatch(edges, start, end, componentsCount).compute(graph);
+        })
+        .flatMap(Set::stream)
+        .collect(Collectors.toSet());
 
     return bridges;
   }
